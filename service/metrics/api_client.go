f8144e76fbcb30d2c2e23f549fd6b4ae686a3905
ralvescosta <rafael.rac.mg@gmail.com>
2023-07-09 17:17:19 -0300

    feat: metrics resource structure

--- service/metrics/api_client.go
+++ service/metrics/api_client.go
@@ -1,22 +1,37 @@
 package metrics
 
 import (
+	"github.com/ralvescosta/emqx-sdk-go/configs"
 	"github.com/ralvescosta/emqx-sdk-go/credentials"
+	"github.com/ralvescosta/emqx-sdk-go/endpoint"
+	"go.uber.org/zap"
 )
 
 type (
 	Options struct {
-		Credentials *credentials.CredentialsProvider
+		Logger      *zap.SugaredLogger
+		Credentials credentials.CredentialsProvider
+		Endpoint    endpoint.EndpointResolver
+		HTTPClient  configs.HTTPClient
 	}
 
 	MetricsClient interface{}
-	Client        struct{}
+	Client        struct {
+		options Options
+	}
 )
 
 func New(options *Options) *Client {
 	return nil
 }
 
-func NewFromConfig() *Client {
-	return nil
+func NewFromConfig(cfg *configs.Config) *Client {
+	opts := &Options{
+		Logger:      cfg.Logger,
+		Credentials: cfg.Credentials,
+		Endpoint:    cfg.Endpoint,
+		HTTPClient:  cfg.HTTPClient,
+	}
+
+	return New(opts)
 }
