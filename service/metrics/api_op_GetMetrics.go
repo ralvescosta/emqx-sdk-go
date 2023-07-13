4f2fcd3d411c84152e9e2a3b15d2d232279dfbe1
ralvescosta <rafael.rac.mg@gmail.com>
2023-07-13 06:52:12 -0300

    feat: metrics endpoint

--- service/metrics/api_op_GetMetrics.go
+++ service/metrics/api_op_GetMetrics.go
@@ -8,27 +8,27 @@ import (
 	"github.com/ralvescosta/emqx-sdk-go/service/metrics/types"
 )
 
-func (c *Client) GetMetrics(ctx context.Context) error {
+func (c *Client) GetMetrics(ctx context.Context) ([]*types.MetricsResponse, error) {
 	url, err := c.options.Endpoint.ResolveEndpoint()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	url.Path = "/api/v5/metrics"
 
 	credentials, err := c.options.Credentials.Retrieve(ctx)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	req, err := http.NewRequest(http.MethodGet, url.String(), nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	secHeader, err := credentials.Header()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	req.Header.Set("content-type", "application/json")
@@ -36,17 +36,15 @@ func (c *Client) GetMetrics(ctx context.Context) error {
 
 	resp, err := c.options.HTTPClient.Do(req)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	body := []*types.MetricsResponse{}
 	json.NewDecoder(resp.Body).Decode(&body)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	println(body)
-
-	return nil
+	return body, nil
 }
