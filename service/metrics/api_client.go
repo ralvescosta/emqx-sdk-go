77e9346c462cfd93c05a91ae4c9ec6d146d44cac
ralvescosta <rafael.rac.mg@gmail.com>
2023-07-15 07:10:24 -0300

    feat: improve client

--- service/metrics/api_client.go
+++ service/metrics/api_client.go
@@ -1,6 +1,11 @@
 package metrics
 
 import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/url"
+
 	"github.com/ralvescosta/emqx-sdk-go/configs"
 	"github.com/ralvescosta/emqx-sdk-go/credentials"
 	"github.com/ralvescosta/emqx-sdk-go/endpoint"
@@ -35,4 +40,47 @@ func NewFromConfig(cfg *configs.Config) *Client {
 	}
 
 	return New(opts)
+}
+
+func (c *Client) perform(ctx context.Context, path string, queryParams *url.Values, resBody interface{}) error {
+	u, err := c.options.Endpoint.ResolveEndpoint()
+	if err != nil {
+		return err
+	}
+
+	u.Path = path
+	if queryParams != nil {
+		u.RawQuery = queryParams.Encode()
+	}
+
+	credentials, err := c.options.Credentials.Retrieve(ctx)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return err
+	}
+
+	secHeader, err := credentials.Header()
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("content-type", "application/json")
+	req.Header.Set("authorization", secHeader)
+
+	resp, err := c.options.HTTPClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	json.NewDecoder(resp.Body).Decode(&resBody)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
