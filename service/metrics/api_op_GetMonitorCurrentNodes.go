e18fc710dbb1e9cbc8ce88a7e252d8f74cf5e47a
ralvescosta <rafael.rac.mg@gmail.com>
2023-07-15 07:11:33 -0300

    feat: monitor current node endpoint

--- service/metrics/api_op_GetMonitorCurrentNodes.go
+++ service/metrics/api_op_GetMonitorCurrentNodes.go
@@ -2,59 +2,24 @@ package metrics
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"net/http"
 
 	"github.com/ralvescosta/emqx-sdk-go/service/metrics/types"
 )
 
-func (c *Client) GetMonitorCurrentNode(ctx context.Context, node string) (*types.MonitorCurrentNodeResponse, error) {
-	if node == "" {
+func (c *Client) GetMonitorCurrentNode(ctx context.Context, nodeParam string) (*types.MonitorCurrentNodeResponse, error) {
+	if nodeParam == "" {
 		return nil, errors.New("node parameter is required")
 	}
 
-	url, err := c.options.Endpoint.ResolveEndpoint()
-	if err != nil {
-		return nil, err
-	}
-
-	url.Path = fmt.Sprintf("/api/v5/monitor_current/nodes/%v", node)
-
-	credentials, err := c.options.Credentials.Retrieve(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequest(http.MethodGet, url.String(), nil)
-	if err != nil {
-		return nil, err
-	}
-
-	secHeader, err := credentials.Header()
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("content-type", "application/json")
-	req.Header.Set("authorization", secHeader)
-
-	resp, err := c.options.HTTPClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
+	urlPath := fmt.Sprintf("/api/v5/monitor_current/nodes/%v", nodeParam)
 	body := &types.MonitorCurrentNodeResponse{}
-	json.NewDecoder(resp.Body).Decode(&body)
+
+	err := c.perform(ctx, urlPath, nil, body)
 	if err != nil {
 		return nil, err
 	}
 
 	return body, nil
-}
-
-func do() {
-
 }
