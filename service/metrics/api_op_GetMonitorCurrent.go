481c4f1caf06d707e21eed70db874aa949e07bb5
ralvescosta <rafael.rac.mg@gmail.com>
2023-07-15 07:11:21 -0300

    feat: monitor current endpoint

--- service/metrics/api_op_GetMonitorCurrent.go
+++ service/metrics/api_op_GetMonitorCurrent.go
@@ -1,1 +1,19 @@
 package metrics
+
+import (
+	"context"
+
+	"github.com/ralvescosta/emqx-sdk-go/service/metrics/types"
+)
+
+func (c *Client) GetMonitorCurrent(ctx context.Context) (*types.MonitorCurrentResponse, error) {
+	urlPath := "/api/v5/monitor_current"
+	body := &types.MonitorCurrentResponse{}
+
+	err := c.perform(ctx, urlPath, nil, body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
