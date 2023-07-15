8f3c9649f8a5b8c112ee0ab26ef782b0cc421a0b
ralvescosta <rafael.rac.mg@gmail.com>
2023-07-15 07:11:27 -0300

    feat: monitor nodes endpoint

--- service/metrics/api_op_GetMonitorNodes.go
+++ service/metrics/api_op_GetMonitorNodes.go
@@ -1,1 +1,34 @@
 package metrics
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/url"
+
+	"github.com/ralvescosta/emqx-sdk-go/service/metrics/types"
+)
+
+func (c *Client) GetMonitorNodes(ctx context.Context, nodeParam, latestQueryParam string) ([]*types.MonitorNodesResponse, error) {
+	if latestQueryParam == "" {
+		return nil, errors.New("latest query parameter is required")
+	}
+
+	if nodeParam == "" {
+		return nil, errors.New("node parameter is required")
+	}
+
+	body := []*types.MonitorNodesResponse{}
+
+	urlPath := fmt.Sprintf("/api/v5/monitor/nodes/%v", nodeParam)
+
+	query := &url.Values{}
+	query.Add("latest", latestQueryParam)
+
+	err := c.perform(ctx, urlPath, query, body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
