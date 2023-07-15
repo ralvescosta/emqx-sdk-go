04ad1c769eb244f5d1a1c431070a4da2bb2c8f50
ralvescosta <rafael.rac.mg@gmail.com>
2023-07-15 07:10:47 -0300

    feat: metrics example

--- examples/metrics/go.mod
+++ examples/metrics/go.mod
@@ -3,12 +3,17 @@ module github.com/ralvescosta/emqx-sdk-go/examples/metrics
 go 1.20
 
 require (
-	github.com/ralvescosta/emqx-sdk-go v0.0.0-20230710093546-4ebd6dbd24e0 // indirect
-	github.com/ralvescosta/emqx-sdk-go/service/metrics v0.0.0-20230710093546-4ebd6dbd24e0 // indirect
+	github.com/ralvescosta/dotenv v1.0.4
+	github.com/ralvescosta/emqx-sdk-go v0.0.0-20230715012117-435122ddbd95
+	github.com/ralvescosta/emqx-sdk-go/service/metrics v0.0.0-20230715012117-435122ddbd95
+)
+
+require (
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.24.0 // indirect
 )
 
 replace github.com/ralvescosta/emqx-sdk-go => ../../
+
 replace github.com/ralvescosta/emqx-sdk-go/service/metrics => ../../service/metrics
--- examples/metrics/main.go
+++ examples/metrics/main.go
@@ -2,18 +2,39 @@ package main
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/ralvescosta/dotenv"
 	"github.com/ralvescosta/emqx-sdk-go/configs"
 	"github.com/ralvescosta/emqx-sdk-go/service/metrics"
 )
 
-func Do() {
+func GetMetrics() {
 	client := metrics.NewFromConfig(configs.DefaultConfig())
 
 	ctx := context.Background()
-	client.GetMetrics(ctx)
+	res, err := client.GetMetrics(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("GetMetrics: %v", res)
+}
+
+func GetMonitorCurrentNode() {
+	client := metrics.NewFromConfig(configs.DefaultConfig())
+
+	ctx := context.Background()
+	res, err := client.GetMonitorCurrentNode(ctx, "emqx@node1.emqx.io")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("\nGetMonitorCurrentNode: %v", *res)
 }
 
 func main() {
-
+	dotenv.Configure(".env")
+	GetMetrics()
+	GetMonitorCurrentNode()
 }
