742cdb09fac1f783f7644330ba71000076ac1472
ralvescosta <rafael.rac.mg@gmail.com>
2023-08-06 14:18:12 -0300

    feat: move authenticator

--- examples/authentication/go.mod
+++ examples/authentication/go.mod
@@ -1,3 +1,17 @@
 module github.com/ralvescosta/emqx-sdk-go/examples/authentication
 
 go 1.20
+
+require (
+	github.com/ralvescosta/dotenv v1.0.4
+	github.com/ralvescosta/emqx-sdk-go v0.0.0-20230805105905-f125aba8a22c
+	github.com/ralvescosta/emqx-sdk-go/service/authentication v0.0.0-20230805105905-f125aba8a22c
+)
+
+require (
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.24.0 // indirect
+)
+
+replace github.com/ralvescosta/emqx-sdk-go/service/authentication => ../../service/authentication
--- examples/authentication/main.go
+++ examples/authentication/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ralvescosta/dotenv"
+	"github.com/ralvescosta/emqx-sdk-go/configs"
+	"github.com/ralvescosta/emqx-sdk-go/service/authentication"
+)
+
+func MoveAuthenticator() {
+	client := authentication.NewFromConfig(configs.DefaultConfig())
+
+	ctx := context.Background()
+
+	err := client.PutMoveAuthenticator(ctx, "password_based", "before:password_based:built_in_database")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print("PutAuthenticationPosition")
+}
+
+func main() {
+	dotenv.Configure(".env")
+
+	MoveAuthenticator()
+}
