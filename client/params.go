dc8e8affbd896b44534707cdd31a6b6e4d226d6d
ralvescosta <rafael.rac.mg@gmail.com>
2023-09-08 17:55:30 -0300

    feat: lint

--- client/params.go
+++ client/params.go
@@ -33,38 +33,45 @@ func NewParams() *Params {
 func (p *Params) Post(path string) *Params {
 	p.method = http.MethodPost
 	p.path = path
+
 	return p
 }
 
 func (p *Params) Get(path string) *Params {
 	p.method = http.MethodGet
 	p.path = path
+
 	return p
 }
 
 func (p *Params) Put(path string) *Params {
 	p.method = http.MethodPut
 	p.path = path
+
 	return p
 }
 
 func (p *Params) Delete(path string) *Params {
 	p.method = http.MethodDelete
 	p.path = path
+
 	return p
 }
 
 func (p *Params) Query(params *url.Values) *Params {
 	p.queryParams = params
+
 	return p
 }
 
 func (p *Params) RequestBody(body io.Reader) *Params {
 	p.request.body = body
+
 	return p
 }
 
 func (p *Params) RequestContentType(contentType string) *Params {
 	p.request.contentType = contentType
+
 	return p
 }
